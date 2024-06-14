613a07dbb783c408dadbd3d4f6d8d009bf615e18
eBdevelpment <ebezerradevelopment@gmail.com>
2024-06-14 08:31:16 -0300

    projeto criado

---
+++ src/main/java/com/edivan/fontes/spring_bible_api/SpringBibleApiApplication.java
@@ -0,0 +1,13 @@
+package com.edivan.fontes.spring_bible_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBibleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBibleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edivan/fontes/spring_bible_api/SpringBibleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edivan.fontes.spring_bible_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBibleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
