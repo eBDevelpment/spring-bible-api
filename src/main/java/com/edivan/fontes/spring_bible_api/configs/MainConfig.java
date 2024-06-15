f835e8741d14f75f57767093223b341383618ea8
eBdevelpment <ebezerradevelopment@gmail.com>
2024-06-15 19:02:13 -0300

    iniciando com logs para a aplicação

--- src/main/java/com/edivan/fontes/spring_bible_api/configs/MainConfig.java
+++ src/main/java/com/edivan/fontes/spring_bible_api/configs/MainConfig.java
@@ -1,18 +1,21 @@
 package com.edivan.fontes.spring_bible_api.configs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MainConfig implements CommandLineRunner {
+	private static Logger logger = LoggerFactory.getLogger(MainConfig.class); 
 
 	@Value("${spring.profiles.active}")
 	private String activeProfile;		
 	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Profile ativo: " + activeProfile);
+		logger.info("Profile ativo: " + activeProfile);
 	}
 
 }
